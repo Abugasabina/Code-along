a7b73cfd85a770976a8c6d96e997bed58e4ee30d
Abuga Sabina <abugasabina26@gmail.com>
2022-08-12 09:30:06 +0000

    code-along

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Header'
+import { Button1,Button2,Button3 } from './components/Button'
+import { Image } from './components/Image'
+
+const App = () => {
+  return (
+    <div>
+      <Image/>
+      <Header/>
+      <Button1/>
+      <Button2/>
+      <Button3/>
+    </div>
+  )
+}
+
+export default App
